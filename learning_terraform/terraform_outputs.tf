32275f441122ceaf9c3a97f643b6819da1862967
jass <your@email.com>
2023-05-22 15:46:09 +0530

    updated

--- learning_terraform/terraform_outputs.tf
+++ learning_terraform/terraform_outputs.tf
@@ -7,3 +7,8 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.jass_app_server.public_ip
 }
+
+output "instance_public_ipv4 DNS" {
+  description = "Public IPv4 DNS address of the EC2 instance"
+  value       = aws_instance.jass_app_server.public_dns
+}
