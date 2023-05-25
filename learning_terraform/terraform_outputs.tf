dba9109e4574dc6370e4e2bbef65da64f47dec9c
jass <your@email.com>
2023-05-25 16:41:28 +0530

    jass

--- learning_terraform/terraform_outputs.tf
+++ learning_terraform/terraform_outputs.tf
@@ -3,6 +3,7 @@ output "instance_id" {
   value       = aws_instance.jass_app_server.id
 }
 
+
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.jass_app_server.public_ip
