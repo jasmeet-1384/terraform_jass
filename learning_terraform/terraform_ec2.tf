e35d6ff6860c84f79464c991920d91d328c6128f
jass <your@email.com>
2023-05-25 10:32:04 +0530

    jass

--- learning_terraform/terraform_ec2.tf
+++ learning_terraform/terraform_ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "jass_app_server" {
   ami           = "ami-0b08bfc6ff7069aff"
   instance_type = "t2.micro"
-  user_data = file("${path.module}/jass.sh")
+  user_data     = file("${path.module}/jass.sh")
 
   tags = {
     Name = var.instance_name
