62dcb7f02c0219517d0ab04348388e965d8098d1
jass <your@email.com>
2023-05-26 16:08:08 +0530

    test

--- learning_terraform/terraform_outputs.tf
+++ learning_terraform/terraform_outputs.tf
@@ -14,36 +14,36 @@
 #   value       = aws_instance.jass_app_server.public_dns
 # }
 
-# # Output -For Loop with List (This will work with count)
-output "for_output_list" {
-  description = "For Loop with list"
-  value = [for instance in aws_instance.jass_app_server: instance.public_ip]
+# # # Output -For Loop with List (This will work with count)
+# output "for_output_list" {
+#   description = "For Loop with list"
+#   value = [for instance in aws_instance.jass_app_server: instance.public_ip]
   
-}
+# }
 
-# Output - For Loop with Map
-output "for_output_map1" {
-  description = "For Loop with Map"
-  value = {for instance in aws_instance.jass_app_server: instance.id => instance.public_dns}
-}
+# # Output - For Loop with Map
+# output "for_output_map1" {
+#   description = "For Loop with Map"
+#   value = {for instance in aws_instance.jass_app_server: instance.id => instance.public_dns}
+# }
 
-# Output - For Loop with Map Advanced
-output "for_output_map2" {
-  description = "For Loop with Map - Advanced"
-  value = {for c, instance in aws_instance.jass_app_server: c => instance.public_dns}
-}
+# # Output - For Loop with Map Advanced
+# output "for_output_map2" {
+#   description = "For Loop with Map - Advanced"
+#   value = {for c, instance in aws_instance.jass_app_server: c => instance.public_dns}
+# }
 
-# Output Legacy Splat Operator (latest) - Returns the List
-output "legacy_splat_instance_publicdns" {
-  description = "Legacy Splat Expression"
-  value = aws_instance.jass_app_server.*.public_dns
-}
+# # Output Legacy Splat Operator (latest) - Returns the List
+# output "legacy_splat_instance_publicdns" {
+#   description = "Legacy Splat Expression"
+#   value = aws_instance.jass_app_server.*.public_dns
+# }
 
-# Output Latest Generalized Splat Operator - Returns the List
-output "latest_splat_instance_publicdns" {
-  description = "Generalized Splat Expression"
-  value = aws_instance.jass_app_server[*].public_dns
-}
+# # Output Latest Generalized Splat Operator - Returns the List
+# output "latest_splat_instance_publicdns" {
+#   description = "Generalized Splat Expression"
+#   value = aws_instance.jass_app_server[*].public_dns
+# }
 
 # EC2 Instance Public IP with TOSET
 output "instance_public_ip" {
@@ -67,7 +67,7 @@ output "instance_public_ip" {
 output "instance_public_dns_map" {
   description = "EC2 Instance with public DNS Maps"
   value = tomap({
-    for s, instance in aws_instance.jass_app_server : s => jass_app_server.public_dns 
+    for s, jass_app_server in aws_instance.jass_app_server : s => jass_app_server.public_dns 
     # S intends to be a subnet ID
   })
   
