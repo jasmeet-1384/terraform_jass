49ba5bf7991594f826b8fe4ba5b28586067dd9b3
jass <your@email.com>
2023-05-26 12:00:38 +0530

    test

--- learning_terraform/terraform_outputs.tf
+++ learning_terraform/terraform_outputs.tf
@@ -4,15 +4,15 @@ output "instance_id" {
 }
 
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.jass_app_server.public_ip
-}
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.jass_app_server.public_ip
+# }
 
-output "instance_public_ipv4_DNS" {
-  description = "Public IPv4 DNS address of the EC2 instance"
-  value       = aws_instance.jass_app_server.public_dns
-}
+# output "instance_public_ipv4_DNS" {
+#   description = "Public IPv4 DNS address of the EC2 instance"
+#   value       = aws_instance.jass_app_server.public_dns
+# }
 
 # # Output -For Loop with List (This will work with count)
 output "for_output_list" {
