8a1aba021134d43ab84201d575b7a19242d4fa3f
jass <your@email.com>
2023-05-26 12:01:22 +0530

    test

--- learning_terraform/terraform_outputs.tf
+++ learning_terraform/terraform_outputs.tf
@@ -1,7 +1,7 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.jass_app_server.id
-}
+# output "instance_id" {
+#   description = "ID of the EC2 instance"
+#   value       = aws_instance.jass_app_server.id
+# }
 
 
 # output "instance_public_ip" {
