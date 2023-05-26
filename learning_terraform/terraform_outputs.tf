7cb097c3169674877ccf6ba0f979e72e14ac0063
jass <your@email.com>
2023-05-26 11:30:20 +0530

    test

--- learning_terraform/terraform_outputs.tf
+++ learning_terraform/terraform_outputs.tf
@@ -4,10 +4,10 @@ output "instance_id" {
 }
 
 
-# output "instance_public_ip" {
-#   description = "Public IP address of the EC2 instance"
-#   value       = aws_instance.jass_app_server.public_ip
-# }
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.jass_app_server.public_ip
+}
 
 output "instance_public_ipv4_DNS" {
   description = "Public IPv4 DNS address of the EC2 instance"
@@ -17,6 +17,6 @@ output "instance_public_ipv4_DNS" {
 # Output -For Loop with List
 output "for_output_list" {
   description = "For loop with list"
-  value = [for instance in aws_instance.jass_app_server: instance.public_ip ]
+  value = [for instance in aws_instance.jass_app_server : instance.public_ip ]
 }
 
