10d4e9318f09227f19fcdf712f9573540b017e0d
jass <your@email.com>
2023-06-02 17:44:53 +0530

    test

--- learning_terraform/terraform_vpc.tf
+++ learning_terraform/terraform_vpc.tf
@@ -6,16 +6,16 @@ module "vpc" {
 
   # VPC Basic Details
   name = var.vpc_name
-  cidr = var.vpc_cidr
+  cidr = var.vpc_cidr_block
 
   azs             = var.availability_zone
-  private_subnets = var.vpc_private_subnet
-  public_subnets  = var.vpc_public_subnet
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
 
   # Database Subnets
   create_database_subnet_group       = var.vpc_create_database_subnet_group
   create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
-  database_subnets                   = var.vpc_database_subnet
+  database_subnets                   = var.vpc_database_subnets
 
   # NAT Gateways - Outbound Communication
   enable_nat_gateway = var.vpc_enable_nat_gateway
