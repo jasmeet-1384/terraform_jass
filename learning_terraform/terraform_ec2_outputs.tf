673c03946e74d503cf70db5cddbed9198c0ec610
jass <your@email.com>
2023-06-12 16:07:00 +0530

    jasmeet

--- learning_terraform/terraform_ec2_outputs.tf
+++ learning_terraform/terraform_ec2_outputs.tf
@@ -11,9 +11,9 @@ output "ec2_bastion_public_ip" {
 # Private EC2 Instances
 output "ec2_private_instance_ids" {
   description = "List of IDs of instances"
-  value       = module.ec2_private.id
+  value       = module.ec2_private[0].id
 }
 output "ec2_private_ip" {
   description = "List of private ip address assigned to the instances"
-  value       = module.ec2_private.private_ip
+  value       = module.ec2_private[*].private_ip
 }
