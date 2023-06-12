a47096b637e413b3efd55e1de795566a0f77ee0c
jass <your@email.com>
2023-06-12 17:24:02 +0530

    jasmeet

--- learning_terraform/terraform_ec2_private.tf
+++ learning_terraform/terraform_ec2_private.tf
@@ -10,9 +10,8 @@ module "ec2_private" {
   user_data     = file("${path.module}/jass.sh")
   vpc_security_group_ids = [module.security-group-private.security_group_id]
   count                  = var.vm_count
-  subnet_id = [
-    module.vpc.private_subnets[0]
-    ]
+  subnet_id =  module.vpc.private_subnets[0]
+    
     
   
   tags = local.common_tags
