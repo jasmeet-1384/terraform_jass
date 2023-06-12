ad75163e12bef2d77f03a6bba15264763a3867b8
jass <your@email.com>
2023-06-12 18:32:07 +0530

    jasmeet

--- learning_terraform/terraform_securityGroup_outputs.tf
+++ learning_terraform/terraform_securityGroup_outputs.tf
@@ -1,7 +1,7 @@
 # Public Bastion Host Security Group Outputs
 output "public_bastion_sg_group_id" {
   description = "The ID of the security group"
-  value = module.sg-bastion-public.this_security_group_id
+  value = module.sg-bastion-public.security_group_id
 }
 output "public_bastion_sg_group_vpc_id" {
   description = "The VPC ID"
@@ -9,19 +9,19 @@ output "public_bastion_sg_group_vpc_id" {
 }
 output "public_bastion_sg_group_name" {
   description = "The name of the security group"
-  value       = module.sg-bastion-public.this_security_group_name
+  value       = module.sg-bastion-public.security_group_name
 }
 
 # Private EC2 Instances Security Group Outputs
 output "private_sg_group_id" {
   description = "The ID of the security group"
-  value       = module.security-group-private.this_security_group_id
+  value       = module.security-group-private.security_group_id
 }
 output "private_sg_group_vpc_id" {
   description = "The VPC ID"
-  value       = module.security-group-private.this_security_group_vpc_id
+  value       = module.security-group-private.security_group_vpc_id
 }
 output "private_sg_group_name" {
   description = "The name of the security group"
-  value       = module.security-group-private.this_security_group_name
+  value       = module.security-group-private.security_group_name
 }
