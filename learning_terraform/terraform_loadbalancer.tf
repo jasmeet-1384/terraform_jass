6c4f06c95c5176d68fff67055c5904240a1176ec
jass <your@email.com>
2023-06-13 15:32:17 +0530

    jass

--- learning_terraform/terraform_loadbalancer.tf
+++ learning_terraform/terraform_loadbalancer.tf
@@ -40,8 +40,7 @@ module "elb" {
   // ELB attachments
   number_of_instances = var.private_instance_count
   instances = [
-    module.ec2_private.id[0],
-    module.ec2_private.id[1]
+    module.ec2_private.id
   ]
 
   tags = local.common_tags
